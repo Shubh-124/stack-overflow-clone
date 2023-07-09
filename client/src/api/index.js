b78f1e418e8031bda65635deb212f4f473bfe0e0
Shubh-124 <baahubali932@gmail.com>
2023-07-09 10:46:12 +0530

    committed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stack-overflow-clone-eldu.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
